a9229c92dce0e998f97cc52267f51da6c2dde1fb
drra1nee <semen.sabetov@yandex.ru>
2026-03-19 11:08:50 +0300

    update models

---
+++ app/models/__init__.py
@@ -0,0 +1,8 @@
+from app.models.user import User
+from app.models.contest import Contest
+from app.models.team import Team
+from app.models.criterion import Criterion
+from app.models.grade import Grade
+from app.models.contest_expert import ContestExpert
+
+__all__ = ['User', 'Contest', 'Team', 'Criterion', 'Grade', 'ContestExpert']
