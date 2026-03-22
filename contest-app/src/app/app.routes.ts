e333d0cda67cae91e7db3fd97ad088b0d4e2019c
polisha <polinazuravlik2@gmail.com>
2026-03-22 19:37:10 +0300

    страница ошибки 404

--- contest-app/src/app/app.routes.ts
+++ contest-app/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { ContestListComponent } from './features/contests/contest-list/contest-list.component';
+import { NotFound } from './features/not-found/not-found';
 
 export const routes: Routes = [
-  { path: '', component: ContestListComponent }
+  { path: '', component: ContestListComponent },
+  { path: '404', component: NotFound },
+  { path: '**', redirectTo: '/404' }
 ];
