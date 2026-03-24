d76c222c3f7634ba7cb998fa59155dd6edc11ca8
polisha <polinazuravlik2@gmail.com>
2026-03-24 23:26:00 +0300

    временный переход на ошибку

--- contest-app/src/app/app.routes.ts
+++ contest-app/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { NotFound } from './features/not-found/not-found';
 
 export const routes: Routes = [
   { path: '', component: ContestListComponent },
+  { path: 'login', component: NotFound },  // временно ведет на 404
   { path: '404', component: NotFound },
   { path: '**', redirectTo: '/404' }
+  
 ];
--- contest-app/src/app/features/contests/contest-list/contest-list.component.ts
+++ contest-app/src/app/features/contests/contest-list/contest-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contest-list',
@@ -8,11 +9,25 @@ import { CommonModule } from '@angular/common';
   templateUrl: './contest-list.component.html',
   styleUrls: ['./contest-list.component.scss']
 })
+
+
 export class ContestListComponent {
+
+  constructor(private router: Router) {}
+
   activeContests = [
     { id: 1, title: 'хакатон 1', year: '2025', image: 'assets/images/photo.jpg' },
     { id: 2, title: 'хакатон 2', year: '2025', image: 'assets/images/photo.jpg' },
     { id: 3, title: 'хакатон 3', year: '2025', image: 'assets/images/photo.jpg' }
   ];
+
+  goToLogin() {  
+    this.router.navigate(['/login']);
+  }
+
+  goToRegister() {  
+    this.router.navigate(['/register']);
+  }
+
 }
 
